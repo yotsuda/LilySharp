a31679bd82986077b248f572b38037e454a06c59
yotsuda <ytsuda@gmail.com>
2026-06-11 23:49:59 +0900

    chore: deploy extension 0.1.2-dev.33 (segno/coda glyph fix)

--- LilySharp.Lsp/LilySharpLanguageServer.cs
+++ LilySharp.Lsp/LilySharpLanguageServer.cs
@@ -32,7 +32,7 @@ namespace LilySharp.Lsp;
 public sealed class LilySharpLanguageServer
 {
     // Version: increment this when making changes to verify deployment
-    public const string Version = "0.1.1-20260611-2332";
+    public const string Version = "0.1.1-20260611-2348";
 
     private readonly JsonRpc _rpc;
     private readonly DocumentManager _documentManager = new();
@@ -1959,6 +1959,7 @@ public class RenderInfo
     public string Type { get; set; } = "";  // "score" or "audio"
     public string Filename { get; set; } = "";
 }
+
 
 
 
