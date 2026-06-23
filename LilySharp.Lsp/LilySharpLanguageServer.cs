7d06124e1b59bf3b0fee3f89544a8177958da66a
yotsuda <ytsuda@gmail.com>
2026-06-24 00:53:21 +0900

    Deploy: bump extension to 0.1.2-dev.80 + LSP version (per-system key signature live)

--- LilySharp.Lsp/LilySharpLanguageServer.cs
+++ LilySharp.Lsp/LilySharpLanguageServer.cs
@@ -32,7 +32,7 @@ namespace LilySharp.Lsp;
 public sealed class LilySharpLanguageServer
 {
     // Version: increment this when making changes to verify deployment
-    public const string Version = "0.1.1-20260624-0032";
+    public const string Version = "0.1.1-20260624-0051";
 
     private readonly JsonRpc _rpc;
     private readonly DocumentManager _documentManager = new();
