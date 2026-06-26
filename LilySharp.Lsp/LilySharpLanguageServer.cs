72f6accf5c3a0e75f07dd09c9ab57bce658860b0
yotsuda <ytsuda@gmail.com>
2026-06-27 00:50:00 +0900

    Bump extension dev version

--- LilySharp.Lsp/LilySharpLanguageServer.cs
+++ LilySharp.Lsp/LilySharpLanguageServer.cs
@@ -32,7 +32,7 @@ namespace LilySharp.Lsp;
 public sealed class LilySharpLanguageServer
 {
     // Version: increment this when making changes to verify deployment
-    public const string Version = "0.1.1-20260627-0005";
+    public const string Version = "0.1.1-20260627-0047";
 
     private readonly JsonRpc _rpc;
     private readonly DocumentManager _documentManager = new();
@@ -2036,6 +2036,7 @@ public class RenderInfo
     public string Type { get; set; } = "";  // "score" or "audio"
     public string Filename { get; set; } = "";
 }
+
 
 
 
