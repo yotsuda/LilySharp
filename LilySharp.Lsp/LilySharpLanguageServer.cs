fc11afb8f6a3a0052c65fdf4c1362eb9cfb0eb77
yotsuda <ytsuda@gmail.com>
2026-06-29 00:40:00 +0900

    Bump dev build version (0.1.2-dev.183)

--- LilySharp.Lsp/LilySharpLanguageServer.cs
+++ LilySharp.Lsp/LilySharpLanguageServer.cs
@@ -35,7 +35,7 @@ namespace LilySharp.Lsp;
 public sealed class LilySharpLanguageServer
 {
     // Version: increment this when making changes to verify deployment
-    public const string Version = "0.1.1-20260628-2345";
+    public const string Version = "0.1.1-20260629-0653";
 
     private readonly JsonRpc _rpc;
     private readonly DocumentManager _documentManager = new();
@@ -2362,6 +2362,7 @@ public class ExportResponse
     public string? OutputPath { get; set; }
     public string? Error { get; set; }
 }
+
 
 
 
