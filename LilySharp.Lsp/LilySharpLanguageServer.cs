2fd22aaed73b57e1214f4ed7e649ab0206e81fa3
yotsuda <ytsuda@gmail.com>
2026-07-01 06:39:49 +0900

    chore(vscode): deploy 0.1.2-dev.198 (bump LSP version marker to verify repeat-barline fix deployment)

--- LilySharp.Lsp/LilySharpLanguageServer.cs
+++ LilySharp.Lsp/LilySharpLanguageServer.cs
@@ -35,7 +35,7 @@ namespace LilySharp.Lsp;
 public sealed class LilySharpLanguageServer
 {
     // Version: increment this when making changes to verify deployment
-    public const string Version = "0.1.1-20260630-1012";
+    public const string Version = "0.1.2-20260630-1900-repeatbarline";
 
     private readonly JsonRpc _rpc;
     private readonly DocumentManager _documentManager = new();
